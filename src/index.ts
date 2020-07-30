8cd6739d3472d5e21c8a2129f53dc19e387bebbc
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-31 00:56:20 +0800

    fix: typing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import type * as Types from "./types";
-
-export *  from "./create-pull-request";
-
-export namespace createPullRequest {
-  export type Options = Types.Options;
-  export type Changes = Types.Changes;
-  export type File = Types.File;
-  export type UpdateFunctionFile = Types.UpdateFunctionFile;
-  export type UpdateFunction = Types.UpdateFunction;
-}
+export * from "./types";
+export * from "./create-pull-request";
